7b021ee28595c72e9ec13a6202d854d3c7bcad0d
Evandro Douglas Capovilla Junior <evandrocapovillajr@gmail.com>
2022-09-14 11:28:09 -0300

    changed to class

--- flappy-bird/src/index.js
+++ flappy-bird/src/index.js
@@ -1,57 +1,27 @@
 
 import Phaser from "phaser";
+import PlayScene from "./scenes/PlayScene";
+
+const WIDTH = 800;
+const HEIGHT = 600;
+const BIRD_POSITION = {x: WIDTH * 0.1, y: HEIGHT / 2};
+
+const SHARED_CONFIG = {
+  widht: WIDTH,
+  height: HEIGHT,
+  startPosition: BIRD_POSITION
+}
 
 const config = {
   type: Phaser.AUTO,
-  width: 800,
-  height: 600,
+  ...SHARED_CONFIG,
   physics: {
     default: 'arcade',
     arcade: {
       debug: true,
-      gravity: { y: 200 }
     }
   },
-  scene: {
-    preload: preload,
-    create: create,
-    update: update
-  }
+  scene: [new PlayScene(SHARED_CONFIG)]
 };
 
 new Phaser.Game(config);
-
-const VELOCITY = 200;
-
-let bird = null;
-let flapVelocity = 150;
-let totalDelta = null;
-const initialBirdPosition = {x: config.width * 0.10, y: config.height/2}
-
-function preload () {
-  this.load.image('sky', 'assets/sky.png');
-  this.load.image('bird', 'assets/bird.png');
-}
-
-function create () {
-  this.add.image(0, 0, 'sky').setOrigin(0);
-  bird = this.physics.add.sprite(initialBirdPosition.x, initialBirdPosition.y, 'bird').setOrigin(0);
-
-  this.input.on('pointerdown', flap)
-}
-
-function update (time, delta) {
-  if (bird.y > config.height || bird.y < -bird.height){
-    restartPlayerPosition()
-  }
-}
-
-function restartPlayerPosition () {
-  bird.x = initialBirdPosition.x;
-  bird.y = initialBirdPosition.y;
-  bird.body.velocity.y = 200;
-}
-
-function flap (){
-  bird.body.velocity.y = -flapVelocity;
-}
